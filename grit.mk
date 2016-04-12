e7b537e1b3f4ed41dab5bd1c4809d2e678b65cbf
Dave Cunningham <sparkprime@gmail.com>
2016-04-11 23:10:20 -0400

    Refactor build system

---
+++ grit.mk
@@ -0,0 +1,16 @@
+UTIL_CPP_SRCS= \
+	colour_conversion.cpp \
+	console.cpp \
+	io_util.cpp \
+	lua_stack.cpp \
+	lua_utf8.cpp \
+	lua_util.cpp \
+	posix_sleep.cpp \
+	unicode_util.cpp \
+
+UTIL_INCLUDE_DIRS= \
+	. \
+
+UTIL_LDLIBS= \
+	-lrt \
+
