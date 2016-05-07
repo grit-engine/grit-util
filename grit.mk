171c513fc314b7133e17060783b25d1192c620fb
Dave Cunningham <sparkprime@gmail.com>
2016-05-07 18:16:03 -0400

    Add include files

--- grit.mk
+++ grit.mk
@@ -8,6 +8,23 @@ UTIL_CPP_SRCS= \
 	posix_sleep.cpp \
 	unicode_util.cpp \
 
+UTIL_INCLUDE_SRCS= \
+	centralised_log.h \
+	colour_conversion.h \
+	console.h \
+	exception.h \
+	intrinsics.h \
+	io_util.h \
+	lua_stack.h \
+	lua_utf8.h \
+	lua_util.h \
+	lua_wrappers_common.h \
+	math_util.h \
+	portable_io.h \
+	sleep.h \
+	spline_table.h \
+	unicode_util.h \
+
 UTIL_INCLUDE_DIRS= \
 	. \
 
